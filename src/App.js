38054bdb7aafe929b3d4fa5fb7aa8c447bdfe906
Andrzej Stasinski <andis_as@wp.pl>
2021-02-21 16:14:52 +0100

    initial app

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const Comp = () => React.createElement('h1', {}, 'App component')
+
+ReactDOM.render(React.createElement(Comp), document.getElementById('root'))
+
+
+
+
+
+
+
