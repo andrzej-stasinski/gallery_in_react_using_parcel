47f2724f9c89fc9fd0a207e9f8c4a59ad6e24e29
Andrzej Stasinski <andis_as@wp.pl>
2021-02-21 21:18:54 +0100

    data flow among components

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,8 @@ const styles = {
     img: {
         width: 150,
         marginBottom: 5,
-        marginRight: 5
+        marginRight: 5,
+        cursor: 'pointer',
     },
     imgLarge: {
         width: '100%',
@@ -17,10 +18,11 @@ let img2 = 'https://wallup.net/wp-content/uploads/2018/10/07/19284-cats-animals-
 let img3 = 'https://s3.amazonaws.com/cdn-origin-etr.akc.org/wp-content/uploads/2017/11/20113314/Carolina-Dog-standing-outdoors.jpg'
 let img4 = 'https://thumbs.dreamstime.com/b/cute-hamsters-snuggle-together-warm-box-cute-hamsters-184133341.jpg'
 
-const Image = ({url}) => {
+const Image = ({url, changeImage}) => {
     const handleClick = event => {
-        event.stopPropagation()
+        // event.stopPropagation()
         console.log(url)
+        changeImage(url)
     }
     return (
         <img
@@ -31,48 +33,32 @@ const Image = ({url}) => {
     )
 }
 
-const Showcase = () => {
-    const [url, setUrl] = useState('https://loremflickr.com/320/240/elephant')
+const Showcase = ({urlImg}) => {
     return (
-        <div>
-            <button 
-                onClick={() => setUrl(img1)}
-            >
-                Image One
-            </button>            
-            <button 
-                onClick={() => setUrl(img2)}
-            >
-                Image Two
-            </button>            
-            <button 
-                onClick={() => setUrl(img3)}
-            >
-                Image Three
-            </button>   
-            <button 
-                onClick={() => setUrl(img4)}
-            >
-                Image Four
-            </button>   
-            <br/><br/>         
-            <img src={url} style={styles.imgLarge} />
+        <div>       
+            <img src={urlImg} style={styles.imgLarge} />
         </div>
     )
 }
 
-const App = () => (
-    <div onClick={event => console.log('click')}>
+const App = () => {
+    const [imageUrl, setImageUrl] = useState('https://loremflickr.com/320/240/elephant')
+    const changeImage = (url) => {
+        setImageUrl(url)
+    }
+    return (
+    <div>
         <h2>Gallery</h2>
-        <Image url={img1} />
-        <Image url={img2} />
-        <Image url={img3} />
-        <Image url={img4} />
+        <Image url={img1} changeImage={changeImage} />
+        <Image url={img2} changeImage={changeImage} />
+        <Image url={img3} changeImage={changeImage} />
+        <Image url={img4} changeImage={changeImage} />
         <div>
-            <Showcase />
+            <Showcase urlImg={imageUrl}/>
         </div>
     </div>
     )
+}
 
 ReactDOM.render(<App />, document.getElementById('root'))
 
