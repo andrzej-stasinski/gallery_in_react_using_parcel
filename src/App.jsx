5ea701062d88eaba5c960ba5f1b7df4bf86eb889
Andrzej Stasinski <andis_as@wp.pl>
2021-02-21 16:29:54 +0100

    add images with props

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,24 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-const Image = () => (
-        <img src="https://hatrabbits.com/wp-content/uploads/2017/01/random.jpg" alt="elephant" />
+const styles = {
+    img: {
+        width: 200,
+        display: 'block',
+        marginBottom: 5
+    }
+}
+const Image = (props) => (
+        <img src={props.srcImg} alt="elephant" style={styles.img} />
     )
 
 const App = () => (
     <>
         <h2>Gallery</h2>
-        <Image />
-        <Image />
+        <Image srcImg="https://hatrabbits.com/wp-content/uploads/2017/01/random.jpg"/>
+        <Image srcImg='https://loremflickr.com/320/240/dog' />
+        <Image srcImg='https://loremflickr.com/320/240/cat' />
+        <Image srcImg='https://loremflickr.com/320/240/chamster' />
     </>
     )
 
