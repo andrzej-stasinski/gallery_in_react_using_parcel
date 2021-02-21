4c8ec121fdb9607545ef10fdeab3b543a9346d0f
Andrzej Stasinski <andis_as@wp.pl>
2021-02-21 21:22:50 +0100

    dynamic data from array

--- src/App.jsx
+++ src/App.jsx
@@ -13,14 +13,8 @@ const styles = {
     }
 }
 
-let img1 = 'https://static.independent.co.uk/s3fs-public/thumbnails/image/2020/02/08/17/botswana-elephant.jpg?width=1200'
-let img2 = 'https://wallup.net/wp-content/uploads/2018/10/07/19284-cats-animals-kittens-faces-748x561.jpg'
-let img3 = 'https://s3.amazonaws.com/cdn-origin-etr.akc.org/wp-content/uploads/2017/11/20113314/Carolina-Dog-standing-outdoors.jpg'
-let img4 = 'https://thumbs.dreamstime.com/b/cute-hamsters-snuggle-together-warm-box-cute-hamsters-184133341.jpg'
-
 const Image = ({url, changeImage}) => {
     const handleClick = event => {
-        // event.stopPropagation()
         console.log(url)
         changeImage(url)
     }
@@ -42,21 +36,33 @@ const Showcase = ({urlImg}) => {
 }
 
 const App = () => {
+
+    const ArrayImg = [
+        'https://static.independent.co.uk/s3fs-public/thumbnails/image/2020/02/08/17/botswana-elephant.jpg?width=1200',
+        'https://wallup.net/wp-content/uploads/2018/10/07/19284-cats-animals-kittens-faces-748x561.jpg',
+        'https://s3.amazonaws.com/cdn-origin-etr.akc.org/wp-content/uploads/2017/11/20113314/Carolina-Dog-standing-outdoors.jpg',
+        'https://thumbs.dreamstime.com/b/cute-hamsters-snuggle-together-warm-box-cute-hamsters-184133341.jpg',
+    ]
+
     const [imageUrl, setImageUrl] = useState('https://loremflickr.com/320/240/elephant')
     const changeImage = (url) => {
         setImageUrl(url)
     }
+    const imageComp = ArrayImg.map(ImgArr => (
+        <Image 
+            key={ImgArr}
+            url={ImgArr} 
+            changeImage={changeImage} 
+        />
+    ))
     return (
-    <div>
-        <h2>Gallery</h2>
-        <Image url={img1} changeImage={changeImage} />
-        <Image url={img2} changeImage={changeImage} />
-        <Image url={img3} changeImage={changeImage} />
-        <Image url={img4} changeImage={changeImage} />
         <div>
-            <Showcase urlImg={imageUrl}/>
+            <h2>Gallery</h2>
+            { imageComp }
+            <div>
+                <Showcase urlImg={imageUrl}/>
+            </div>
         </div>
-    </div>
     )
 }
 
